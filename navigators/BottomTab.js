7525749035612643ad2d743f48447388ee7016c1
Arnav2401 <77386547+Arnav2401@users.noreply.github.com>
2021-08-14 21:03:34 +0530

    Update BottomTab.js

--- navigators/BottomTab.js
+++ navigators/BottomTab.js
@@ -4,28 +4,33 @@ import Feed from '../screens/feed';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AntDesign } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-const Tab = createBottomTabNavigator()
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name='Feed' component={Feed} options={{
-                tabBarLabel: 'Feed',
-                tabBarIcon: ({ color, size, focused }) => {
-                    return(
-                    <AntDesign name="picture" size={24} color="black" />
-                )}
-            }}
-            />
-            <Tab.Screen name='Create' component={Create} options={{
-                tabBarLabel: 'Create',
-                tabBarIcon: ({ color, size, focused }) => {
-                    return(
-                    <Ionicons name="create" size={24} color="black" />                }
-            )}} 
-            />
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({ color, size, focused }) => {
+            return <AntDesign name="picture" size={24} color="black" />;
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Create"
+        component={Create}
+        options={{
+          tabBarLabel: 'Create',
+          tabBarIcon: ({ color, size, focused }) => {
+            return <Ionicons name="create" size={24} color="black" />;
+          },
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-export default TabNavigator
+export default TabNavigator;
